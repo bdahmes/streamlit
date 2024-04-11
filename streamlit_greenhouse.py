118fc6a412856527b5a30418ef7336e50994d32b
Bryan Dahmes <BDahmes@BallHort.com>
2024-04-11 10:06:51 -0500

    Problem downloading xlsx on public app

--- streamlit_greenhouse.py
+++ streamlit_greenhouse.py
@@ -309,7 +309,7 @@ def to_excel(df):
     #worksheet = writer.sheets['Results']
     #format1 = workbook.add_format({'num_format': '0.00'}) 
     #worksheet.set_column('A:A', None)  
-    writer.save()
+    writer.close()
     processed_data = output.getvalue()
     return processed_data
 
